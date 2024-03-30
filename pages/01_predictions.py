f1a88eaeb5bf4c8d057e2eb7849a1bfa53d62100
Sushant H Vema <sashikanth@Sushants-MBP.local>
2024-03-29 18:23:13 -0700

    authorization code as HF secret, use os.getenv, manually initialize

--- pages/01_predictions.py
+++ pages/01_predictions.py
@@ -9,6 +9,13 @@ import ipywidgets as widgets
 from pathlib import Path
 import sys
 
+
+import os
+
+AUTHORIZATION_CODE = os.getenv("EARTHENGINE_TOKEN")
+ee.Authenticate(authorization_code=AUTHORIZATION_CODE)
+ee.Initialize(project="gee-sand")
+
 # read in the dataset file json
 
 # dataset_path  = "/static/public/dataset.json"
